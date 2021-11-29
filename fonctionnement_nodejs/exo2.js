3cebc6f65f98f85d295cb36e921cacb6e756fef0
Antony Correia <antonycorreia@MBP-de-Antony.home>
2021-11-29 23:00:29 +0100

    update

--- fonctionnement_nodejs/exo2.js
+++ fonctionnement_nodejs/exo2.js
@@ -4,43 +4,48 @@ const ss = require('simple-statistics')
 
 const csvToJson = require('convert-csv-to-json');
 
-const filepath = process.argv[2] ? process.argv[2] : ""
-const columname = process.argv[3] ? process.argv[3] : ""
+const filePath = process.argv[2] ? process.argv[2] : ""
+const columnName = process.argv[3] ? process.argv[3] : ""
 let json;
 
-if (!findFile(filepath)) {
+if (!fs.existsSync(filePath)) {
     console.log('File does not exists')
 } else {
-    if (path.extname(filepath) === '.csv') {
-        console.log(path.basename(filepath))
-        json = csvToJson.getJsonFromCsv(filepath);
+    if (path.extname(filePath) === '.csv') {
+        console.log(path.basename(filePath))
+        json = csvToJson.getJsonFromCsv(filePath);
         json.forEach((element, index) => {
             if (checkLine(Object.values(element))) {
                 json.splice(index, 1)
             }
         })
-        max(columname)
-    }
-    else {
+        max(columnName)
+    } else {
         console.log('Erreur de path')
     }
 }
 
-function findFile(filepath) {
-    if (fs.existsSync(filepath)) {
-        return true
-    }
-    else {
-        return false;
-    }
+function writeFile(nameFile, columnName, data) {
+    fs.writeFile(`statistique_${nameFile}_${columnName}.txt`, data, (err) => {
+        if (err)
+            console.log(err);
+        else {
+            console.log("File written successfully\n");
+            console.log("The written has the following contents:");
+            console.log(fs.readFileSync(`statistique_${nameFile}_${columnName}.txt`, "utf8"));
+        }
+    });
 }
 
-function max(columname) {
+function max(columnName) {
     let tab = []
     for (let element of json) {
-        tab.push(parseInt(element[columname]))
+        tab.push(parseInt(element[columnName]))
     }
-    console.log(`Max ${columname} : ${ss.max(tab)} `)
+    const data = `Max ${columnName} : ${ss.max(tab)}`;
+    const nameFile = 'max'
+    console.log(data)
+    writeFile(nameFile, columnName, data);
 }
 
 function checkLine(line) {
